8f7127ae98ae23efd195d28c031ee833b98a3171
Kimseungsuu <107012988+Kimseungsuu@users.noreply.github.com>
2022-07-14 19:50:59 +0900

    과제완료!!!

--- src/main/java/com/example/bloghome/member/MemerController.java
+++ src/main/java/com/example/bloghome/member/MemerController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-//@RestController
 public class MemerController {
 
     private final MemberService memberService;
